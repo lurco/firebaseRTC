f78f80a5f0d9e9179fabf9fe4390ceda5e93439e
Andrzej Legutko <andrzejlegutko@gmail.com>
2024-05-16 11:23:17 +0200

    add files

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: 'https://lurco.github.io/firebaseRTC/',
+    build: {
+        outDir: 'build'
+    }
+})
